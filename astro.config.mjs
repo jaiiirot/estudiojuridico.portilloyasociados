b44062d55ef0ffbc2c574c4b53e85cf29f6b0f57
jhon jairo pastor <jhon.jairo.tumiri@gmail.com>
2026-04-10 09:20:46 -0300

    Fix: actualizacion de adaptador vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
